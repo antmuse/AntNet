d347dcbe1fb7c9e2a37d7377cb4d9d7ec165890d
antmuse <antmuse@live.cn>
2018-09-01 12:58:05 +0800

    better logger

--- Include/Public/HEngineLog.h
+++ Include/Public/HEngineLog.h
@@ -7,22 +7,31 @@ namespace irr {
 
 /// Enum of all supported log levels in Legend.
 enum ELogLevel {
-	ELOG_DEBUG,
-	ELOG_INFO,
-	ELOG_WARNING,
-	ELOG_ERROR,
-	ELOG_CRITICAL,
-	ELOG_COUNT
+    ELOG_DEBUG,
+    ELOG_INFO,
+    ELOG_WARNING,
+    ELOG_ERROR,
+    ELOG_CRITICAL,
+    ELOG_COUNT
 };
 
 /// Contains strings for each log level to make them easier to print to a stream.
-const c8* const LogLevelStrings[] = {
-	"Debug",
-	"Information",
-	"Warning",
-	"Error",
-	"Critical",
-	0
+const c8* const AppLogLevelNames[] = {
+    "Debug",
+    "Information",
+    "Warning",
+    "Error",
+    "Critical",
+    0
+};
+
+const wchar_t* const AppWLogLevelNames[] = {
+    L"Debug",
+    L"Information",
+    L"Warning",
+    L"Error",
+    L"Critical",
+    0
 };
 
 } //namespace irr
--- Source/Public/CConsoleLogReceiver.cpp
+++ Source/Public/CConsoleLogReceiver.cpp
@@ -1,5 +1,5 @@
 #include "CConsoleLogReceiver.h"
-#include "irrString.h"
+#include <stdio.h>
 
 #if defined(APP_PLATFORM_ANDROID)
 #include <android/log.h>
@@ -22,9 +22,9 @@ CConsoleLogReceiver::~CConsoleLogReceiver() {
 
 bool CConsoleLogReceiver::log(ELogLevel level, const c8* pSender, const c8* pMessage) {
 #if defined(APP_PLATFORM_ANDROID)
-    __android_log_print(level + ANDROID_LOG_DEBUG, "NeatSpark", "[%s]  %s>    %s\n", LogLevelStrings[level], pSender, pMessage);
+    __android_log_print(level + ANDROID_LOG_DEBUG, "NeatSpark", "[%s]  %s>    %s\n", AppLogLevelNames[level], pSender, pMessage);
 #else
-    printf("[%s]  %s>    %s\n", LogLevelStrings[level], pSender, pMessage);
+    printf("[%s]  %s>    %s\n", AppLogLevelNames[level], pSender, pMessage);
 #endif
     return true;
 }
@@ -33,9 +33,9 @@ bool CConsoleLogReceiver::log(ELogLevel level, const c8* pSender, const c8* pMes
 
 bool CConsoleLogReceiver::log(ELogLevel level, const wchar_t* pSender, const wchar_t* pMessage) {
 #if defined(APP_PLATFORM_ANDROID)
-    __android_log_print(level + ANDROID_LOG_DEBUG, "NeatSpark", "[%s]  %s>    %s\n", LogLevelStrings[level], pSender, pMessage);
+    __android_log_print(level + ANDROID_LOG_DEBUG, "NeatSpark", "[%s]  %s>    %s\n", AppLogLevelNames[level], pSender, pMessage);
 #else
-    wprintf(L"[%s]  %s>    %s\n", core::stringw(LogLevelStrings[level]).c_str(), pSender, pMessage);
+    wprintf(L"[%s]  %s>    %s\n", AppWLogLevelNames[level], pSender, pMessage);
 #endif
     return true;
 }
