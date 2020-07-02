f69be1c69556e48914ae44b4d85f990210527b98
antmuse <antmuse@live.cn>
2020-07-02 13:58:32 +0800

    time manager

---
+++ Source/Core/CTimeManager.cpp
@@ -0,0 +1,54 @@
+#include "CTimeManager.h"
+#include "CTimer.h"
+
+namespace app {
+namespace core {
+
+
+CTimeManager::CTimeManager(s32 step) :
+    mStep(step < 1 ? 1 : (step > 2000 ? 2000 : step)),
+    mRunning(false),
+    mTimer(CTimer::getRelativeTime(), 100) {
+}
+
+
+CTimeManager::~CTimeManager() {
+}
+
+
+void CTimeManager::run() {
+    s64 last;
+    s64 curr;
+    for (; mRunning;) {
+        curr = CTimer::getRelativeTime();
+        mTimer.update(curr);
+        last = curr - last;
+        if (last < mStep) {
+            CThread::sleep(mStep - last);
+        }
+        last = curr;
+    }
+}
+
+
+void CTimeManager::start() {
+    if (!mRunning) {
+        mRunning = true;
+        mTimer.setCurrent(CTimer::getRelativeTime());
+        mThread.start(*this);
+    }
+}
+
+
+void CTimeManager::stop() {
+    if (mRunning) {
+        mRunning = false;
+        mThread.join();
+        mTimer.clear();
+    }
+}
+
+
+
+}//namespace core
+}//namespace app
